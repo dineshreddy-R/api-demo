59cd7c1fbe8972fefa191a754fdd7df23354edad
Rajasekhar <tatireddyr7887@gmail.com>
2025-05-27 12:52:17 +0530

    db related file

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
+import json
+import psycopg2
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
+dine_db_host = "pg-12c7bdf1-reddy94.l.aivencloud.com"
+db_name = "defaultdb"
+user = "avnadmin"
+pwd = "AVNS_Xtm8bKHSR1GtTelNyMC"
+port = "15563"
 
 
+@app.route("/")
+def index_page():
+    return "hello from index"
 
-@app.route('/')
+@app.route("/")
 def dine():
-    return"this is flask"
+    conn = psycopg2.connect(
+        host=dine_db_host,
+        dbname=db_name,
+        user=user,
+        password=pwd,
+        port=port
+    )
+    print("connection created")
+    cur = conn.cursor()
+
+    sql = "select * from student"
+    cur.execute(sql)
+    result = cur.fetchall()
+    # Get column names
+    colnames = [desc[0] for desc in cur.description]
+
+    # Convert to list of dictionaries
+    students = [dict(zip(colnames, row)) for row in result]
+    cur.close()
+    conn.close()
+    return json.dumps(students)
 
 
-if __name__=='__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ dine_db.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+dine_db_host = "pg-12c7bdf1-reddy94.l.aivencloud.com"
+db_name = "defaultdb"
+user = "avnadmin"
+pwd = "AVNS_Xtm8bKHSR1GtTelNyMC"
+port = "15563"
+
+"""
+DB related stuff
+1. create connection
+2. create cursor
+3. run sql
+4. close connection --> check in GPT
+"""
+
+conn = psycopg2.connect(
+    host=dine_db_host,
+    dbname=db_name,
+    user=user,
+    password=pwd,
+    port=port
+)
+print("connection created")
+cur = conn.cursor()
+
+sql = "select * from student"
+cur.execute(sql)
+result = cur.fetchall()
+conn.close()
+
+"""for i in result:
+    print(i)
+    print('#'*6)"""
