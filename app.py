e715cdb92d0ec81d05aa6ccbc42251a1d1f4bcd9
Rajasekhar <tatireddyr7887@gmail.com>
2025-05-28 11:13:05 +0530

    env update

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 import json
+import os
+
 import psycopg2
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-dine_db_host = "pg-12c7bdf1-reddy94.l.aivencloud.com"
-db_name = "defaultdb"
-user = "avnadmin"
-pwd = "AVNS_Xtm8bKHSR1GtTelNyMC"
-port = "15563"
+dine_db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+user = os.getenv("DB_USER")
+pwd = os.getenv("DB_PASSWORD")
+port = os.getenv("DB_PORT")
 
+@app.route("/login")
+def login_page():
+    # username paswd check here
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register_page():
+    return render_template("register.html")
 
 @app.route("/")
 def index_page():
-    return "hello from index"
+    return render_template("index.html")
 
-@app.route("/")
+
+@app.route("/api/students")
 def dine():
     conn = psycopg2.connect(
         host=dine_db_host,
